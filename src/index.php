5884bdf4b43abd0a4343ed3a0a59e90cb31826ce
JanSterle1337 <jan.sterle123@gmail.com>
2022-07-08 13:46:52 +0200

    code improvement

--- src/index.php
+++ src/index.php
@@ -1,38 +1,15 @@
 <?php
 require_once '../vendor/autoload.php';
 require '../classes/Route.php';
-
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 $templates = new League\Plates\Engine(__DIR__ . '/views');
 
 $request = Request::createFromGlobals();
 $path = $request->getPathInfo();
-echo "<pre>";
-var_dump($path);
-echo "</pre>"; 
-
-
-/*
-function getPath() {
-    $path = $_SERVER['REQUEST_URI'] ?? '/';
-    $position = strpos($path, '?');
-    if ($position === false) {
-        return $path;
-    } 
-
-    return $path = substr($path,0,$position);
-
-}
-
-$path = getPath();
-echo "My path: " . $path . "</br>";
-*/
 
 $router = new Route();
 $router->set($path,'funkcija');
-
 
 if (in_array($path, ['', '/'])) {
     $response = new Response($templates->render('Home'));
@@ -41,4 +18,5 @@ if (in_array($path, ['', '/'])) {
 } else {
     $response = new Response('Page not found.', Response::HTTP_NOT_FOUND);
 }
+
 $response->send();
